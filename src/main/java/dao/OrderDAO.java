aa69ac9cc08500bb1e55e73eb026320374dae8a6
Ignacioscript <ignacio.script@gmail.com>
2024-08-04 11:52:04 -0500

    OrderDAO basic SQL Query Structure created

--- src/main/java/dao/OrderDAO.java
+++ src/main/java/dao/OrderDAO.java
@@ -1,10 +1,21 @@
 package dao;
 
 import model.Order;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.List;
 
 public class OrderDAO extends DataAccessObject<Order>{
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderDAO.class);
+
+    private final String INSERT = "";
+    private final String UPDATE = "";
+    private final String DELETE = "";
+    private final String GET_ONE = "";
+    private final String GET_ALL = "";
+
     @Override
     public void create(Order order) {
 
