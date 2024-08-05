28b0bcc33c9f29e5c42584eb583ff65e692e944a
Aka-Shinjuku <shinjuku.codejin@gmail.com>
2024-08-05 17:06:01 -0500

    Services logic created

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -1,4 +1,44 @@
 package service;
 
-public class OrderService {
+import dao.DataAccessObject;
+import dao.OrderDAO;
+import model.Order;
+
+import java.util.List;
+
+public class OrderService extends DataAccessObject<Order> {
+
+
+    private  OrderDAO orderDAO;
+
+    public OrderService() {
+        this.orderDAO = new OrderDAO();
+    }
+
+
+    @Override
+    public void create(Order order) {
+        orderDAO.create(order);
+
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return orderDAO.getAll();
+    }
+
+    @Override
+    public Order getById(int id) {
+        return orderDAO.getById(id);
+    }
+
+    @Override
+    public void update(Order order) {
+        orderDAO.update(order);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        orderDAO.deleteById(id);
+    }
 }
--- src/main/java/service/TitleService.java
+++ src/main/java/service/TitleService.java
@@ -1,4 +1,43 @@
 package service;
 
-public class TitleService {
+import dao.DataAccessObject;
+import dao.TitleDAO;
+import model.Title;
+
+import java.util.List;
+
+public class TitleService extends DataAccessObject<Title> {
+
+
+    private final TitleDAO titleDAO;
+
+    public TitleService() {
+        this.titleDAO = new TitleDAO();
+    }
+
+    @Override
+    public void create(Title title) {
+        titleDAO.create(title);
+
+    }
+
+    @Override
+    public List<Title> getAll() {
+        return titleDAO.getAll();
+    }
+
+    @Override
+    public Title getById(int id) {
+        return titleDAO.getById(id);
+    }
+
+    @Override
+    public void update(Title title) {
+        titleDAO.update(title);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        titleDAO.deleteById(id);
+    }
 }
